0e3d3c1a4f1ca545d55f401f2763a93ce5305523
agnesvarga1 <agikolyok@gmail.com>
2024-01-09 17:01:59 +0100

    feat dynamic grid + fizzBuzz Style added

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+const title = document.querySelector("h1");
+let mainContainer = document.createElement("div");
+
+mainContainer.classList.add("container");
+
+title.append(mainContainer);
+//print fizz buzz in console based on the remainders of the numb/s from 1 to 100
 for (let i = 1; i <= 100; i++) {
+  let singleCell = document.createElement("div");
+  singleCell.classList.add("cell");
+  mainContainer.appendChild(singleCell);
   if (i % 5 === 0 && i % 3 === 0) {
     console.log("FizzBuzz");
+    singleCell.innerHTML = "FizzBuzz";
+    singleCell.classList.add("fizz-buzz");
   } else if (i % 5 === 0) {
     console.log("Buzz");
+    singleCell.innerHTML = "Buzz";
+    singleCell.classList.add("buzz");
   } else if (i % 3 === 0) {
     console.log("Fizz");
+    singleCell.innerHTML = "Fizz";
+    singleCell.classList.add("fizz");
   } else {
     console.log(i);
+    singleCell.innerHTML = i;
+    singleCell.classList.add("normal");
   }
 }
